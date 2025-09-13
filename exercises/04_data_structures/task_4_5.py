84e26ffeaf4f43c7d60530dc154fb44a70c2053d
stromilov <stromilovnikolay@gmail.com>
2025-09-13 17:15:30 +0300

    task 5 done and pass autotest

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -22,3 +22,6 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+
+result = list(set(command1.split()[-1].split(',')).intersection(command2.split()[-1].split(','))).sorted()
+print(result)
