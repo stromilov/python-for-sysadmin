525dab358a8b31b215934dc1ffdcb827cac49a3d
nick <stromilovnikolay@gmail.com>
2025-09-17 19:53:25 +0300

    rewrite task_4_5

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -23,5 +23,5 @@
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
 
-result = list(set(command1.split()[-1].split(',')).intersection(command2.split()[-1].split(','))).sorted()
+result = sorted(list(set(command1.split()[-1].split(',')).intersection(command2.split()[-1].split(','))))
 print(result)
