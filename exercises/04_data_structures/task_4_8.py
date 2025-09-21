e219fa2d8467ef8e3dab8c98bb6e2420173884bd
nick <stromilovnikolay@gmail.com>
2025-09-21 08:08:25 +0300

    task 8 done and pass autotest

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -26,3 +26,10 @@
 """
 
 ip = "192.168.3.1"
+ip_template = '''
+{0:<10} {1:<10} {2:<10} {3:<10}
+{0:08b}   {1:08b}   {2:08b}   {3:08b}
+'''
+ip_mas = ip.split('.')
+
+print(ip_template.format(int(ip_mas[0]), int(ip_mas[1]), int(ip_mas[2]), int(ip_mas[3])))
