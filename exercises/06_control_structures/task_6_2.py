18454c944f3dd9b01adcd8cf827a0ae7c1ba52bf
stromilov <stromilovnikolay@gmail.com>
2025-09-26 12:06:56 +0300

    task6.2 done and pass autotest

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
 Задание 6.2
@@ -12,3 +13,16 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input("Введите ip-адрес в формате 10.0.1.1: ")
+ip_oct = ip.split('.')
+
+if int(ip_oct[0]) >= 1 and int(ip_oct[0]) <= 223:
+    print('unicast')
+elif int(ip_oct[0]) >= 224 and int(ip_oct[0]) <= 239:
+    print('multicast')
+elif int(ip_oct[0]) == 255 and int(ip_oct[1]) == 255 and int(ip_oct[2]) == 255 and int(ip_oct[3]) == 255:
+    print('local broadcast')
+elif int(ip_oct[0]) == 0 and int(ip_oct[1]) == 0 and int(ip_oct[2]) == 0 and int(ip_oct[3]) == 0:
+   print('unassigned')
+else:
+    print('unused')
