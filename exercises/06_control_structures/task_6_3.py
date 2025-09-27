cd07949bdf9363c8a3481ff531804bc9e33e9b63
nick <stromilovnikolay@gmail.com>
2025-09-27 12:28:34 +0300

    start resolve task

--- exercises/06_control_structures/task_6_3.py
+++ exercises/06_control_structures/task_6_3.py
@@ -74,11 +74,14 @@ trunk = {
     "0/7": ["only", "30"],
 }
 
-# for intf, vlan in access.items():
-#     print("interface FastEthernet" + intf)
-#     for command in access_template:
-#         if command.endswith("access vlan"):
-#             print(f" {command} {vlan}")
-#         else:
-#             print(f" {command}")
+intf = input("Interface 0/12: ")
+vlan = input("VLAN: ")
+
+for intf, vlan in access.items():
+    print("interface FastEthernet" + intf)
+    for command in access_template:
+        if command.endswith("access vlan"):
+            print(f" {command} {vlan}")
+        else:
+            print(f" {command}")
 
