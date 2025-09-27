355622ce27a5283c5fef7c665e6b71d465f0bbfc
nick <stromilovnikolay@gmail.com>
2025-09-27 12:27:30 +0300

    start resolve task

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,21 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+ip = input("Введите ip-адрес в формате 10.0.1.1: ")
+ip_oct = ip.split('.')
+
+if (not ip_oct[0].isnumeric() or not ip_oct[1].isnumeric() or not ip_oct[2].isnumeric() or not ip_oct[3].isnumeric()):
+    print("Неправильный IP-адрес")
+
+else:
+   if int(ip_oct[0]) >= 1 and int(ip_oct[0]) <= 223:
+       print('unicast')
+   elif int(ip_oct[0]) >= 224 and int(ip_oct[0]) <= 239:
+       print('multicast')
+   elif int(ip_oct[0]) == 255 and int(ip_oct[1]) == 255 and int(ip_oct[2]) == 255 and int(ip_oct[3]) == 255:
+       print('local broadcast')
+   elif int(ip_oct[0]) == 0 and int(ip_oct[1]) == 0 and int(ip_oct[2]) == 0 and int(ip_oct[3]) == 0:
+      print('unassigned')
+   else:
+       print('unused')
