1eff951776c955ccaccb336a4e56504ac6fb4ed1
nick <stromilovnikolay@gmail.com>
2025-10-04 17:28:58 +0300

    task12.1 done and pass autotest

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Задание 12.1
@@ -17,3 +18,35 @@ IP-адрес считается доступным, если выполнени
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+
+import subprocess
+
+ip_list = ['192.168.0.200',  '8.8.8.8',        '8.8.4.4',
+           '1.1.1.1',        '208.67.222.222', '4.2.2.2',
+           '77.88.8.8',      '9.9.9.9',        '1.0.0.1',
+           '208.67.220.220', '94.140.14.14',   '172.10.11.12']
+
+
+def ping_ip_addresses(ip_list):
+    '''
+    Функция проверяет пингуются ли IP-адреса
+    '''
+
+    allow_ip = []
+    not_allow_ip = []
+
+    for i in ip_list:
+        res = subprocess.run(['ping', '-c', '1', '-n', i], 
+                             stdout=subprocess.PIPE, 
+                             stderr=subprocess.PIPE, 
+                             encoding='utf-8')
+        
+        if res.returncode == 0:    
+            allow_ip.append(i)
+        else:
+            not_allow_ip.append(i)
+
+    return (allow_ip, not_allow_ip)
+
+
+print(ping_ip_addresses(ip_list))
