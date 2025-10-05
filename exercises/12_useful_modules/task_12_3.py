b18cf29b65e1de18f74fa27fdc1094a29318b65c
nick <stromilovnikolay@gmail.com>
2025-10-05 16:31:35 +0300

    task12.3 done with deepseek and pass autotest

--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -18,3 +18,34 @@ Reachable    Unreachable
              10.1.1.9
 
 """
+from tabulate import tabulate
+
+
+reach_ip_list = ['10.1.1.1', '10.1.1.2']
+unreach_ip_list = ['10.1.1.7', '10.1.1.8', '10.1.1.9']
+
+def print_ip_table(reach_ip_list, unreach_ip_list):
+    """
+    Отображает таблицу доступных и недоступных IP-адресов.
+    
+    Args:
+        reachable_ips (list): Список доступных IP-адресов
+        unreachable_ips (list): Список недоступных IP-адресов
+    """
+    
+    ip_table = [('Reachable', 'Unreachable')]
+    
+    # Определяем максимальную длину списков
+    max_length = max(len(reach_ip_list), len(unreach_ip_list))
+    
+    
+    for i in range(max_length):
+        reachable = reach_ip_list[i] if i < len(reach_ip_list) else ""
+        unreachable = unreach_ip_list[i] if i < len(unreach_ip_list) else ""
+        ip_table.append([reachable, unreachable])
+
+
+    print(tabulate(ip_table, headers='firstrow'))
+
+
+print_ip_table(reach_ip_list, unreach_ip_list)
