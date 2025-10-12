f57d11a21d22429ff5044efea005c2cf0897e3d4
nick <stromilovnikolay@gmail.com>
2025-10-12 19:04:05 +0300

    task22.1a done and pass autotest

--- exercises/22_oop_basics/task_22_1a.py
+++ exercises/22_oop_basics/task_22_1a.py
@@ -14,6 +14,18 @@ class Topology:
     def __init__(self, topology_dict):
         self.topology = self._normalize(topology_dict)
 
+    def _normalize(self, topology_dict):
+        normalized_topology = {}
+        seen_links = set()
+
+        for key, value in topology_dict.items():
+            link = tuple(sorted([key, value]))
+            if link not in seen_links:
+                normalized_topology[key] = value
+                seen_links.add(link)
+
+        return normalized_topology
+
 
 topology_example = {
     ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
@@ -26,3 +38,7 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+
+top = Topology(topology_example)
+
